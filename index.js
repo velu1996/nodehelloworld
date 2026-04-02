2d8aa8894482d05b40ee7fdc75736834c06701e2
Vel Kumar P <34476188+velu1996@users.noreply.github.com>
2026-04-02 19:19:01 +0530

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Sowmya!\n'
+  const msg = 'Hello Sowmya! Whats for dinner?\n'
   res.end(msg);
 });
 
